261d7997180f7fbe9361c5799cbeda158079232b
Alvaro Valdebenito <avaldebe@gmail.com>
2018-07-24 22:12:42 +0200

    update INA

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,25 @@ uint8_t devicesFound = 0;
 
 void setup() {
   Serial.begin(115200);
-#ifdef  __AVR_ATmega32U4__
-  delay(2000); // wait 2s in leonardo/micro
-#endif
   devicesFound = INA.begin(1,100000);     // maxBusAmps,microOhmR
   INA.setBusConversion(8500);             // 8.244ms
   INA.setShuntConversion(8500);           // 8.244ms
-  INA.setAveraging(128);                  // samples
+  INA.setAveraging(128);                  // 128 samples ==> ~1 measurement/s
   INA.setMode(INA_MODE_CONTINUOUS_BOTH);  // bus&shunt
 
   Serial.print(F("INA devices on the I2C bus: "));
+  for (uint8_t i=0;i<devicesFound;i++) {
+    Serial.print(i); Serial.print(F(": "));
+    Serial.println(INA.getDeviceName(i));
+  }
+
   Serial.println(devicesFound);
-  Serial.println(F("channel,type,voltage[mV],current[uA]"));
+  Serial.println(F("#,voltage[mV],current[uA]"));
 }
 
 void loop() {
   for (uint8_t i=0;i<devicesFound;i++) {
     Serial.print(i); Serial.print(F(","));
-    Serial.print(INA.getDeviceName(i)); Serial.print(F(","));
     Serial.print(INA.getBusMilliVolts(i)); Serial.print(F(","));
     Serial.println(INA.getBusMicroAmps(i));
   }
