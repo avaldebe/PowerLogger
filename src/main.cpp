7ea5c09a3774d26b25dbe41c42711c33d374c4f8
Alvaro Valdebenito <alvarov@met.no>
2018-07-25 14:59:04 +0200

    buffer to EEPROM

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ Based on
 INA_Class INA;
 uint8_t devicesFound = 0;
 
+#include <EEPROM.h> 
+
 #include <SdFat.h>
 ArduinoOutStream cout(Serial);            // stream to Serial
 SdFat SD;                                 // File system object.
@@ -26,25 +28,42 @@ void setup() {
 
   // list channels
   cout << F("INA devices on the I2C bus\n");
-  for (uint8_t i=0;i<devicesFound;i++) {
+  for (uint8_t i=0; i<devicesFound; i++) {
     cout << F("ch") << i << F(": ") << INA.getDeviceName(i) << endl;
   }
   
   // header
   cout << F("millis");
-  for (uint8_t i=0;i<devicesFound;i++) {
+  for (uint8_t i=0; i<devicesFound; i++) {
     cout << F(",ch") << i << F(" voltage [mV]")
          << F(",ch") << i << F(" current [uA]");
   }
-  cout << endl;
+  cout << F(",") << endl;
 }
 
 void loop() {
+  #ifdef __STM32F1__  // emulated EEPROM
+  uint16  ee;
+  #else
+  uint8_t ee;
+  #endif
+
+  // buffere results to EEPROM after INA devices
+  ee = devicesFound*sizeof(inaEEPROM); // free EEPROM address, after INA devices
+  EEPROM.put(ee, (uint32_t)millis());
+  for (uint8_t i=0; i<devicesFound; i++) {
+    ee += sizeof(uint32_t); EEPROM.put(ee, INA.getBusMilliVolts(i));
+    ee += sizeof(uint32_t); EEPROM.put(ee, INA.getBusMicroAmps(i));
+  }
+
+  ee = devicesFound*sizeof(inaEEPROM); // free EEPROM address, after INA devices
+  uint32_t aux;
   cout << millis();
-  for (uint8_t i=0;i<devicesFound;i++) {
-    cout << F(",") << INA.getBusMilliVolts(i)
-         << F(",") << INA.getBusMicroAmps(i);
+  for (uint8_t i=0; i<(devicesFound*2+1); i++, ee+=sizeof(uint32_t)) {
+    EEPROM.get(ee, aux);
+    cout << aux << F(",");
   }
   cout << endl;
+
   delay(5000);
 }
